3aec6efe9253c267c3ee2697ab01db4588d02b4f
ZwtKevinMac <43810373+ZwtKevinMac@users.noreply.github.com>
2018-10-03 15:19:49 +0530

    added view controller file

---
+++ .gitignore/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Sport B.I.
+//
+//  Created by Kevin on 03/10/18.
+//  Copyright © 2018 Zealousweb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+    }
+}
+
